2fd03f8656bf883f39781cd9dc351029cd253efd
LeeviP <lpkknn@gmail.com>
2022-05-21 16:15:11 +0300

    Add simple movement on the main scene

--- Assets/Scripts/HoverboardMovement.cs
+++ Assets/Scripts/HoverboardMovement.cs
@@ -5,16 +5,37 @@ using UnityEngine;
 public class HoverboardMovement : MonoBehaviour
 {
     [SerializeField]
-    private float _trackPullDistance;
+    private float _hoverHeight = 1;
     [SerializeField]
-    private float _trackPullForce;
+    private float _hoverDamping = 0.25f;
     [SerializeField]
-    private float _hoverHeight;
+    private float _rotationDamping = 0.25f;
     [SerializeField]
-    private float _hoverDamping;
+    private LayerMask _layer;
+    [SerializeField]
+    private float _fallSpeed;
+
+    private bool _grounded;
 
     private void FixedUpdate()
     {
-        
+        _grounded = Physics.Raycast(transform.position, -transform.up, out RaycastHit hit, _hoverHeight * 2, _layer, QueryTriggerInteraction.Ignore);
+
+        if(_grounded)
+        {
+            var toRotation = Quaternion.FromToRotation(transform.up, hit.normal);
+            transform.rotation = Quaternion.Slerp(transform.rotation, toRotation, _rotationDamping);
+
+            transform.position = Vector3.Lerp(transform.position, hit.point + hit.normal * _hoverHeight, _hoverDamping);
+        }
+        else
+        {
+            var toRotation = Quaternion.FromToRotation(transform.up, Vector3.up);
+            transform.rotation = Quaternion.Slerp(transform.rotation, toRotation, _rotationDamping);
+
+            transform.position += Vector3.down * _fallSpeed;
+        }
+
+        transform.position += transform.forward * 0.1f;
     }
 }
